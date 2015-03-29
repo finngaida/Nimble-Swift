1facd3a04f723910f68754bab9bcabb29092be76
Josh Trommel <joshtrommel@gmail.com>
2015-03-28 19:37:59 -0700

    Initial Commit

---
+++ Bright/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Bright
+//
+//  Created by Josh Trommel on 2015-03-28.
+//  Copyright (c) 2015 Intui, Inc. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Bright/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Bright
+//
+//  Created by Josh Trommel on 2015-03-28.
+//  Copyright (c) 2015 Intui, Inc. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
