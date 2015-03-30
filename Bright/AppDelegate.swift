2e8b49372be8f82253ef17fe43cc089bb9fefdd6
Josh Trommel <joshtrommel@gmail.com>
2015-03-29 21:58:44 -0700

    fixed Bright base files before I go ham at this shit

--- Bright/AppDelegate.swift
+++ Bright/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  Bright
 //
-//  Created by Josh Trommel on 2015-03-28.
+//  Created by Josh Trommel on 2015-03-29.
 //  Copyright (c) 2015 Intui, Inc. All rights reserved.
 //
 
@@ -11,6 +11,7 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    @IBOutlet weak var window: NSWindow!
 
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
--- BrightTests/BrightTests.swift
+++ BrightTests/BrightTests.swift
@@ -2,7 +2,7 @@
 //  BrightTests.swift
 //  BrightTests
 //
-//  Created by Josh Trommel on 2015-03-28.
+//  Created by Josh Trommel on 2015-03-29.
 //  Copyright (c) 2015 Intui, Inc. All rights reserved.
 //
 
