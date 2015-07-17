18e50c9e2cf32c1b2e77a862e39b69643c847562
Josh Trommel <joshtrommel@gmail.com>
2015-07-16 22:48:06 -0700

    I'm so sorry Adam

--- Bright/AppDelegate.swift
+++ Bright/AppDelegate.swift
@@ -12,6 +12,26 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
+    
+    var statusBar = NSStatusBar.systemStatusBar()
+    var statusBarItem: NSStatusItem = NSStatusItem()
+    var menu: NSMenu = NSMenu()
+    var menuItem: NSMenuItem = NSMenuItem()
+    
+    @IBAction func showMainWindow(sender: NSStatusBarButton) {
+        NSLog("Query")
+    }
+    
+    override func awakeFromNib() {
+        statusBarItem = statusBar.statusItemWithLength(-1)
+        statusBarItem.menu = menu
+        statusBarItem.title = "Bright"
+        
+        menuItem.action = Selector("showMainWindow:")
+        menuItem.title = "Query"
+        menuItem.keyEquivalent = ""
+        menu.addItem(menuItem)
+    }
 
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
