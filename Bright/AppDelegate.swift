9c3eac879a5f6cbef13da89c6d04b18aa8394dd4
Josh Trommel <joshtrommel@gmail.com>
2015-07-17 01:03:46 -0400

    Initial Commit for Swift version. Adam won't like this

---
+++ Bright/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Bright
+//
+//  Created by Josh Trommel on 2015-07-17.
+//  Copyright (c) 2015 Intui. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
