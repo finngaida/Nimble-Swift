20daf9ee36be0d8b020d72427722006e541c10b9
Josh Trommel <joshtrommel@gmail.com>
2015-09-03 00:37:27 -0700

    Added menus and buttonsopen *x* Removed 'quickmaths' for the time being

--- Nimble/MenuViewController.swift
+++ Nimble/MenuViewController.swift
@@ -15,6 +15,18 @@ class MenuViewController: NSViewController {
     @IBOutlet weak var assumption: NSTextField!
     @IBOutlet weak var plaintext: NSTextField!
     
+    func showAbout(sender: AnyObject) { println("About test") }
+    func showPreferences(sender: AnyObject) { println("Preferences test") }
+    
+    @IBAction func showOptions(sender: NSButton) {
+        var menu = NSMenu()
+        let about = NSMenuItem(title: "About", action: Selector("showAbout:"), keyEquivalent: "")
+        let preferences = NSMenuItem(title: "Preferences", action: Selector("showPreferences:"), keyEquivalent: "")
+        menu.addItem(about)
+        menu.addItem(preferences)
+        NSMenu.popUpContextMenu(menu, withEvent: NSApplication.sharedApplication().currentEvent!, forView: sender as NSView)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         input.target = self
@@ -30,39 +42,33 @@ class MenuViewController: NSViewController {
         let url = NSURL(string: "https://nimble-backend.herokuapp.com/input?i=\(escapedURL!)")
         
         // Before anything happens, check to see if it's a basic math problem we can solve ourselves
-        let expression = NSExpression(format:query)
-        let result = expression.expressionValueWithObject(nil, context: nil) as! NSNumber
+//        let expression = NSExpression(format:query)
+//        let result = expression.expressionValueWithObject(nil, context: nil) as! NSNumber
         
-        if let thing = result as? Int {
-            self.plaintext.stringValue = "\(result)"
-        } else {
-            progress.startAnimation(self)
-            
-            let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {(data, response, error) in
-                let json = JSON(data: data)
-                if json["result"]["success"] == true {
-                    
-                    if let input = json["result"]["input"].string {
-                        self.assumption.stringValue = "Assuming you meant \"\(input)\""
-                    }
-                    
-                    if let result = json["result"]["result"]["plaintext"].string {
-                        self.plaintext.stringValue  = result
-                    }
-                    
-                } else {
-                    let url = json["result"]["origin_url"]
-                    self.assumption.stringValue = "Uh oh! I think your query, \"\(self.input.stringValue)\", might be invalid."
-                    self.plaintext.stringValue = "You might have better luck searching it on Wolfram|Alpha: \(url)"
-                    
+        progress.startAnimation(self)
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {(data, response, error) in
+            let json = JSON(data: data)
+            if json["result"]["success"] == true {
+                
+                if let input = json["result"]["input"].string {
+                    self.assumption.stringValue = "Assuming you meant \"\(input)\""
                 }
-                self.progress.stopAnimation(self)
+                
+                if let result = json["result"]["result"]["plaintext"].string {
+                    self.plaintext.stringValue  = result
+                }
+                
+            } else {
+                let url = json["result"]["origin_url"]
+                self.assumption.stringValue = "Uh oh! I think your query, \"\(self.input.stringValue)\", might be invalid."
+                self.plaintext.stringValue = "You might have better luck searching it on Wolfram|Alpha: \(url)"
+                
             }
-            
-            task.resume()
-
+            self.progress.stopAnimation(self)
         }
         
+        task.resume()
     }
     
 }
